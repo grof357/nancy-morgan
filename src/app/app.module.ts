be19902561bbb5f0e0687a6d63df9c0f15524d46
DESKTOP-CPNL3UH\Djordje <djordjedjurdjic@gmail.com>
2022-01-31 10:22:30 +0100

    cdhh

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponentComponent } from './components/header-component/header-component.component';
-import { NavBarComponentComponent } from './components/nav-bar-component/nav-bar-component.component';
 import { LoginRegisterBandComponent } from './components/login-register-band/login-register-band.component';
 import { SectionAComponent } from './components/section-a/section-a.component';
+import { LogoNavbarComponentComponent } from './components/logo-navbar-component/logo-navbar-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponentComponent,
-    NavBarComponentComponent,
     LoginRegisterBandComponent,
-    SectionAComponent
+    SectionAComponent,
+    LogoNavbarComponentComponent
   ],
   imports: [
     BrowserModule
